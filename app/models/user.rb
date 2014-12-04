6c2ca08be10fd8778b4ec8de9664e88bb616d941
Harry Grillo <harry@redbassett.com>
2014-12-04 22:22:00 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   
   before_save :downcase_email
@@ -64,6 +65,11 @@ class User < ActiveRecord::Base
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  # Defines a proto-feed.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
   private
